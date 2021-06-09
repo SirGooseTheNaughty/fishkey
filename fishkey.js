098e193eddfd831268e01305d6e6d2c7fa6533c7
Sergey Vorobyev <syvorobyev@gmail.com>
2021-06-09 17:53:50 +0300

    fixed uniburger for mobile

--- fishkey.js
+++ fishkey.js
@@ -20,6 +20,11 @@ function getBrowserName() {
       return "unknown";
 }
 
+/* утилита для определения мобильных устройств */
+function isMobile() {
+    return /Mobi/i.test(window.navigator.userAgent);
+}
+
 /* утилита для получения текущего брейкпоинта */
 function getCurrentBreakpoint () {
     const tildaBreakpoints = [1200, 980, 640, 480, 320];
@@ -1367,8 +1372,8 @@ function uniBurger_init(params) {
     const burgerWrapper = document.querySelector('.burgerWrapper');
 
     function burgerReshape() {
-        const ww = $(window).width() + 17,
-            wh = $(window).height(),
+        const ww = isMobile() ? window.screen.width : $(window).width() + 17,
+            wh = isMobile() ? window.screen.height : $(window).height(),
             maxDim = Math.max(ww, wh);
 
         switch (burgerShape) {
